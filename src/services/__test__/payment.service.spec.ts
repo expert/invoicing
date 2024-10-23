50ca02630e6f62d3582a3efb6d8e5c0b2a9da047
alex <fktjif@gmail.com>
2024-10-23 20:04:27 +0300

    [ADD] Setup testing

---
+++ src/services/__test__/payment.service.spec.ts
@@ -0,0 +1,9 @@
+// @vitest-environment node
+import { describe, expect, test } from "vitest";
+
+describe("paymentService", () => {
+  test("example", () => {
+    const a = 10
+    expect(a).toEqual(10)
+  })
+})
---
+++ tests/components/Welcome.nuxt.spec.ts
@@ -0,0 +1,10 @@
+// tests/components/Welcome.nuxt.spec.ts
+import { renderSuspended } from '@nuxt/test-utils/runtime'
+import { Welcome } from '#components'
+import { screen } from '@testing-library/vue'
+import { it, expect } from 'vitest'
+
+it('can render some component', async () => {
+  await renderSuspended(Welcome)
+  expect(screen.getByText('Welcome Alexei')).toBeDefined()
+})
